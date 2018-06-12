c9867143be3e05c859609d3484f798e143ef5c37
yuval hoch <hochyuv@macs.biu.ac.il>
2018-06-12 18:37:47 +0300

    12\06

--- ImageWebApplication/Controllers/WebController.cs
+++ ImageWebApplication/Controllers/WebController.cs
@@ -15,24 +15,19 @@ namespace ImageWebApplication.Controllers
     {
         static AppConfig appConfig = new AppConfig();
         static Logs logs = new Logs();
-        static Approve approve = new Approve();
         static Photos photos = new Photos();
         static ImageWeb imageWeb = new ImageWeb();
         private static string m_handlerToRemove;
-        public object objLock;
 
-
-
-        // GET: First
-        public ActionResult Approve()
-        {
-            return View(approve);
-        }
-
+        /// <summary>
+        /// returns a view for the main window of the project
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult MainView()
         {
             ViewBag.Status = imageWeb.Status;
+            //gets inside condition only in the first time showing this view
             if (imageWeb.Status.Equals("On") && appConfig.InitDone==false)
             {
                 appConfig.WaitWithBlock();
@@ -41,20 +36,37 @@ namespace ImageWebApplication.Controllers
             return View(imageWeb);
         }
 
-
+        /// <summary>
+        /// returns a view for all the photos in the service.
+        /// </summary>
+        /// <returns> the photos view</returns>
         [HttpGet]
         public ActionResult Photos()
         {
+            //reset the old
             photos.PhotosList.Clear();
+            //get all of the photos in the serivce
             photos.GetAllPhotos(appConfig.OutputDir);
             return View(photos.PhotosList);
         }
 
+        /// <summary>
+        /// returns a view which asks if we're sure we want to delete a specific photo
+        /// </summary>
+        /// <param name="fullUrlThumb">The full URL for the photo thumbnail.</param>
+        /// <param name="dir">The output directory of the image.</param>
+        /// <returns></returns>
         public ActionResult PhotoDelete(string fullUrlThumb, string dir)
         {
             PhotoInfo photoToDelete = new PhotoInfo(fullUrlThumb,dir);
             return View(photoToDelete);
         }
+        /// <summary>
+        /// occurs when the user approves to delete a specific photo.
+        /// deletes it and return to the photos view
+        /// </summary>
+        /// <param name="fullThumbUrl">The full thumb URL.</param>
+        /// <returns></returns>
         public ActionResult DeletePhotoOK(string fullThumbUrl)
         {
             photos.DeletePhoto(fullThumbUrl);
@@ -73,11 +85,19 @@ namespace ImageWebApplication.Controllers
             return View(photo);
         }
 
+        /// <summary>
+        /// view for the configuration settings
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Config()
         {
             return View(appConfig);
         }
 
+        /// <summary>
+        /// view the logs
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Logs()
         {
             return View(logs);
@@ -89,14 +109,23 @@ namespace ImageWebApplication.Controllers
             return PartialView("LogsTable", logs.FilterLogs(filter));
         }
 
-        public ActionResult ApproveDeleteHandler(string handlerToRemove)
+        /// <summary>
+        /// Approves the delete handler.
+        /// </summary>
+        /// <param name="handlerToRemove">The handler to remove.</param>
+        /// <returns></returns>
+        public ActionResult Approve(string handlerToRemove)
         {
             m_handlerToRemove = handlerToRemove;
-            return RedirectToAction("Approve");
+            return View();
 
         }
-       
-       public ActionResult DeleteHandlerOK()
+
+        /// <summary>
+        /// occurs when the user approves he wants to delete the handler
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DeleteHandlerOK()
         {
             JObject json = new JObject();
             json["CommandID"] = (int)CommandEnum.CloseCommand;
